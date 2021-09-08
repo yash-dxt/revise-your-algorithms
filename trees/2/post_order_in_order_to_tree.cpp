d7036c4473a42d16e9b85390244c3da57957e348
Yash Dixit <yashdixitsq@gmail.com>
2021-09-09 00:33:41 +0530

    feat: Create tree using post order and in order array

---
+++ trees/2/post_order_in_order_to_tree.cpp
@@ -0,0 +1,46 @@
+
+
+#include <vector>
+
+using namespace std;
+
+//Node of a binary tree.
+class Node
+{
+public:
+    int data;
+    Node *left;
+    Node *right;
+    Node(int x)
+    {
+        data = x;
+    }
+};
+
+// in : Inorder array
+// po: Postorder array
+// l -> left, r -> right.
+
+int currIndex; // Size of array - 1 --> Post order has the first element at the last.
+Node *postOrderInorderToTree(int in[], int po[], int l, int r)
+{
+    if (l > r)
+        return NULL;
+    int elem = po[currIndex++];
+    Node *curr = new Node(elem);
+
+    int x;
+    for (int i = l; i < r; i++)
+    {
+        if (in[i] == elem)
+        {
+            x = i;
+            break;
+        }
+    }
+
+    curr->right = postOrderInorderToTree(in, po, l, x - 1);
+    curr->left = postOrderInorderToTree(in, po, x + 1, r);
+
+    return curr;
+}
